23b47b4540a54a5add54e2a2c3198e1e779893cb
Kenneth Graham <klgraham@gmail.com>
2016-11-27 13:59:12 -0600

    added an update to project 2, as project 6, that uses Autolayout to make the app work in landscape mode

---
+++ Project 6a/Project 2/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Kenneth L. Graham on 11/13/16.
+//  Copyright © 2016 Kenneth Graham. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy",
+                      "monaco", "nigeria", "poland", "russia", "spain",
+                      "uk", "us"]
+
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
+        // chose some flags from the array, put them in buttons
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
+        
+        // prompt the user to select the correct flag and wait for them to do so
+        
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        scoreLabel.text = "Score: \(score)"
+        
+        let alertController = UIAlertController(title: title, message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        self.present(alertController, animated: true)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
