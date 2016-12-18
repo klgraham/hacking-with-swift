7a3723eb426e2264ee9d65a6708de82d8882b297
Kenneth Graham <klgraham@gmail.com>
2016-12-18 14:42:12 -0500

    Added outlets and actions

--- Project 8/Project 8/ViewController.swift
+++ Project 8/Project 8/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoresLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitTapped(_ sender: Any) {
+    }
 
+    @IBAction func clearTapped(_ sender: Any) {
+    }
 }
 
